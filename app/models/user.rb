87751262d236ed832c133dba929edeeee9fc98f9
koki0809 <kouki20180728switch@gmail.com>
2026-03-10 16:22:44 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :prototypes
   has_many :comments
